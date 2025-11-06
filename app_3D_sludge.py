df2cdfa426d83cd0da2a77ba0db36a104863d338
minda-Lai-ai <laimindar@gmail.com>
2025-11-06 12:22:29 +0800

    Update app_3D_sludge.py

--- app_3D_sludge.py
+++ app_3D_sludge.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+from mpl_toolkits.mplot3d import Axes3D
 from scipy.interpolate import griddata
-import plotly.graph_objs as go
+import io        
 
 # 可選的字型處理（Plotly/Streamlit組合下通常直接支援中文）
 st.set_page_config(page_title="油槽內油泥分布圖", layout="wide")
@@ -108,4 +111,5 @@ else:
 
 # 作者資訊
 st.caption("Designed by Minda")
+
 
